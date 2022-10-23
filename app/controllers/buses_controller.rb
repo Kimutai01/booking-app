29f11c2cc51c652fc46bbf0001e9fefd97c69b13
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-23 10:14:49 +0300

    Added buses table

---
+++ app/controllers/buses_controller.rb
@@ -0,0 +1,70 @@
+class BusesController < ApplicationController
+  before_action :set_bus, only: %i[ show edit update destroy ]
+
+  # GET /buses or /buses.json
+  def index
+    @buses = Bus.all
+  end
+
+  # GET /buses/1 or /buses/1.json
+  def show
+  end
+
+  # GET /buses/new
+  def new
+    @bus = Bus.new
+  end
+
+  # GET /buses/1/edit
+  def edit
+  end
+
+  # POST /buses or /buses.json
+  def create
+    @bus = Bus.new(bus_params)
+
+    respond_to do |format|
+      if @bus.save
+        format.html { redirect_to bus_url(@bus), notice: "Bus was successfully created." }
+        format.json { render :show, status: :created, location: @bus }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @bus.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /buses/1 or /buses/1.json
+  def update
+    respond_to do |format|
+      if @bus.update(bus_params)
+        format.html { redirect_to bus_url(@bus), notice: "Bus was successfully updated." }
+        format.json { render :show, status: :ok, location: @bus }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @bus.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /buses/1 or /buses/1.json
+  def destroy
+    @bus.destroy
+
+    respond_to do |format|
+      format.html { redirect_to buses_url, notice: "Bus was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_bus
+      @bus = Bus.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def bus_params
+      params.require(:bus).permit(:registration, :capacity, :from, :to, :departing_time, :date)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_065604) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_071301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buses", force: :cascade do |t|
+    t.string "registration"
+    t.integer "capacity"
+    t.string "from"
+    t.string "to"
+    t.time "departing_time"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
